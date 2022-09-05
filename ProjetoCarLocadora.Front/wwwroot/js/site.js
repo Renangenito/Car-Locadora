a96bae31bfbb03b70e4356fc9d64a7074d46493d
Renan Lima <renangenito@hotmail.com>
2022-09-05 17:26:49 -0300

    Fazendo alguns ajustes

--- ProjetoCarLocadora.Front/wwwroot/js/site.js
+++ ProjetoCarLocadora.Front/wwwroot/js/site.js
@@ -2,5 +2,5 @@
     $('.maskTelefone').inputmask({ mask: ['(99) 9999-9999'] })
     $('.maskCelular').inputmask({ mask: ['(99) 99999-9999'] })
     $('.maskCpf').inputmask({ mask: ['999.999.999-99'] })
-
+    $('.maskPlacaVeiculo').inputmask({ mask: ['aaa-9999'] })
 });
